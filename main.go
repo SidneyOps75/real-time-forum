ba11033f523f516cd9ec2ac6af484f97d0516891
bobaigwa <oikobrian99@gmail.com>
2025-05-09 16:20:23 +0300

    fix:call register handler

--- main.go
+++ main.go
@@ -16,27 +16,28 @@ func main() {
 
 	// Set up API routes
 	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/register", handlers.RegisterHandler)
 	
 	// Serve static files
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// Important: Serve index.html for the root path
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// If it's not an actual file/resource being requested, serve the SPA
 		if r.URL.Path != "/" && r.URL.Path != "/index.html" {
 			// Check if the file exists
 			if _, err := http.Dir("./static").Open(r.URL.Path); err != nil {
-				// File doesn't exist, serve index.html instead
+				
 				http.ServeFile(w, r, "./static/index.html")
 				return
 			}
 		}
-		// Serve the requested file or index.html directly
+		
 		http.ServeFile(w, r, "./static/index.html")
 	})
 
 	// Start server
-	log.Println("Server started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Server started at http://localhost:8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
