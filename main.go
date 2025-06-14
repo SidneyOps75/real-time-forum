51a7f90f2ab08cfb92955265df0cac085d200a44
bobaigwa <oikobrian99@gmail.com>
2025-06-14 16:59:07 +0300

    Add handler for javascript files

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
 	"real/db"
 	"real/handlers"
-	rt_hub"real/websocket"
-	 
-
+	rt_hub "real/websocket"
 )
-
-
 
 func main() {
 	if err := db.Init("./forum.db"); err != nil {
@@ -22,35 +19,40 @@ func main() {
 	hub := rt_hub.NewHub()
 	go hub.Run()
 
-	// Serve static files
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("static/images/"))))
+	// Serve static files with proper MIME types
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("static/images"))))
 
-	
+	// Explicit handler for JavaScript files
+	http.HandleFunc("/js/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		http.ServeFile(w, r, "./static"+r.URL.Path)
+	})
 
-	// Existing Handlers
+	// Existing API handlers
 	http.HandleFunc("/api/categories", handlers.GetCategoriesHandler)
 	http.HandleFunc("/api/posts", handlers.GetPostsHandler)
 	http.HandleFunc("/post/create", handlers.CreatePostHandler)
-	http.HandleFunc("/login", handlers.LoginHandler) 
-	http.HandleFunc("/register", handlers.RegisterHandler) 
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.HandleFunc("/like", handlers.LikeHandler)
 	http.HandleFunc("/comment/create", handlers.CreateCommentHandler)
 	http.HandleFunc("/comment/like", handlers.CommentReactionHandler)
-
-	
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		handlers.ServeWs(hub, w, r)
 	})
 	http.HandleFunc("/api/users", handlers.HandleGetUsers)
 	http.HandleFunc("/api/messages", handlers.HandleGetMessages)
+
+	// Serve index.html for all other routes
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/index.html")
+		if filepath.Ext(r.URL.Path) == "" {
+			http.ServeFile(w, r, "./static/index.html")
+		} else {
+			http.NotFound(w, r)
+		}
 	})
 
-	// Start server
-	log.Println("Server started at http://localhost:9094")
-	log.Fatal(http.ListenAndServe(":9094", nil))
-
+	log.Println("Server started at http://localhost:9008")
+	log.Fatal(http.ListenAndServe(":9008", nil))
 }
